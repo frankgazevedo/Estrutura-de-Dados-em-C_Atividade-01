46ac6c8fce3828a1354d74f8ebe082a7ecf3f539
Frank Azevedo <frankgazevedo@gmail.com>
2022-09-20 12:18:52 -0400

    update: questão 04

--- questao04.c
+++ questao04.c
@@ -12,16 +12,18 @@ Questão 04: Escreva um algoritmo que leia três notas de um aluno e imprime no 
 
 #include <stdio.h>
 
+// número de notas que serão lidas
+#define QTD_NOTAS 3
+
 int main()
 {
     // Declaração e inicialização de Variáveis
     float nota = 0;
     float soma = 0;
     float media = 0;
-    int contador = 3; // número de notas que serão lidas
 
     // Leitura das notas.
-    for (int i = 0; i < contador; i++)
+    for (int i = 0; i < QTD_NOTAS; i++)
     {
         printf("Entre com a nota #%d: ", (i +1));
         scanf(" %f%*[^\n]", &nota);
@@ -31,7 +33,7 @@ int main()
     }
 
     // Calcula a média
-    media = soma / contador;
+    media = soma / QTD_NOTAS;
 
     // Imprimindo os valores das variáveis
     printf("Média: %.2f\n", media);
